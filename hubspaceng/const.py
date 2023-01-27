86f69b85d1a458ac1410c346b75d21d72dd8f9bd
Shawn <shawngmc@gmail.com>
2023-01-26 19:26:18 -0500

    Remove old constants

--- hubspaceng/const.py
+++ hubspaceng/const.py
@@ -9,15 +9,9 @@ BASE_API_ENDPOINT = f"https://{BASE_API_HOST}/v1"
 
 METADATA_API_CALLING_HOST = "semantics2.afero.net"
 METADATA_API_HOST = "api2.afero.net"
-METADATA_API_URL = (
-    "https://api2.afero.net/v1/accounts/{account_id}/metadevices"
-)
 
 USER_AGENT = "Dart/2.15 (dart:io)"
 
 DEFAULT_STATE_UPDATE_INTERVAL = timedelta(seconds=10)
 DEFAULT_TOKEN_REFRESH = 10 * 60  # 10 minutes
 WAIT_TIMEOUT = 60
-
-# DEFAULT_DURATION = 120 * 1000
-# DEFAULT_EXPIRATION_BUFFER = 10 * 1000
